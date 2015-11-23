aa495b8f22ee4b4b9d5889c29145a02b0db6cdf1
arlynch435 <arlynch@NN123-HA-05.HS.sd203.org>
2015-11-23 14:11:44 -0600

    Finished Partial Array

--- Chapter7Practice/PartialArray.java
+++ Chapter7Practice/PartialArray.java
@@ -30,14 +30,16 @@ public class PartialArray
              i<this.currentSize;
              i++)
              {
-                 this.values[i-i]=this.values[i];
+                 this.values[i-1]=this.values[i];
                 }
         this.currentSize--;
     }
     public void insert(int pos, int value)
     {
-        if (this.currentSize<this.values.length)
+        if (this.currentSize==this.values.length)
         {
+            this.growArray();
+        }
             this.currentSize++;
             for (int i=this.currentSize-1;
                  i>pos;
@@ -46,7 +48,31 @@ public class PartialArray
                      this.values[i]=this.values[i-1];
                     }
             this.values[pos]=value;
-        }
+    }
+    public void swap(int posA,int posB)
+    {
+        int temp=this.values[posA];
+        this.values[posA]=this.values[posB];
+        this.values[posB]=temp;
+    }
+    private void growArray()
+    {
+        int totalSize= this.values.length;
+        int[] newArray= new int[totalSize*2];
+        for (int i=0;
+             i<totalSize;
+             i++)
+             {
+                 newArray[i]=this.values[i];
+                }
+        this.values=newArray;
+    }
+    public static void main(String[] args)
+    {
+        PartialArray weee=new PartialArray();
+        weee.remove(5);
+        weee.insert(5,6);
+        weee.swap(5,2);
     }
 
 }
